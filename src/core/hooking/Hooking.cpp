40d6f9ffc1e780acb8238b17fb52817807444b09
𝐶𝑆𝑌𝑂𝑁 <134310111+CSY0N@users.noreply.github.com>
2026-01-19 00:42:50 +0100

    Update Hooking.cpp

--- src/core/hooking/Hooking.cpp
+++ src/core/hooking/Hooking.cpp
@@ -1,3 +1,4 @@
+
 #include "Hooking.hpp"
 
 #include "BaseHook.hpp"
@@ -49,7 +50,6 @@ namespace YimMenu
 		BaseHook::Add<Hooks::Matchmaking::MatchmakingUpdate>(new DetourHook("MatchmakingUpdate", Pointers.MatchmakingUpdate, Hooks::Matchmaking::MatchmakingUpdate));
 		BaseHook::Add<Hooks::Matchmaking::MatchmakingFindSessions>(new DetourHook("MatchmakingFindSessions", Pointers.MatchmakingFindSessions, Hooks::Matchmaking::MatchmakingFindSessions));
 		BaseHook::Add<Hooks::Matchmaking::MatchmakingFindSessionsResponse>(new DetourHook("MatchmakingFindSessionsResponse", Pointers.MatchmakingFindSessionsResponse, Hooks::Matchmaking::MatchmakingFindSessionsResponse));
-		
 		BaseHook::Add<Hooks::Misc::AssistedAimShouldReleaseEntity>(new DetourHook("AssistedAimShouldReleaseEntity", Pointers.AssistedAimShouldReleaseEntity, Hooks::Misc::AssistedAimShouldReleaseEntity));
 		BaseHook::Add<Hooks::Misc::GetLabelText>(new DetourHook("GetLabelText", Pointers.GetLabelText, Hooks::Misc::GetLabelText));
 		BaseHook::Add<Hooks::Misc::GetLabelTextInternal>(new DetourHook("GetLabelTextInternal", Pointers.GetLabelTextInternal, Hooks::Misc::GetLabelTextInternal));
@@ -91,4 +91,3 @@ namespace YimMenu
 		BaseHook::Hooks().clear();
 	}
 }
-
